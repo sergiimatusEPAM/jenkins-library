8dfacc7a635ceca05b8ec6b7bf36726636285f5d
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-07-31 11:37:21 +0200

    fix: raised DCOS_VERSION version to 1.13.3

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -186,7 +186,7 @@ def call() {
             }
             agent { label 'dcos-terraform-cicd' }
             environment {
-              DCOS_VERSION = '1.13.2'
+              DCOS_VERSION = '1.13.3'
               // DCOS_VERSION_UPGRADE = '1.13.1'
               GOOGLE_APPLICATION_CREDENTIALS = credentials('dcos-terraform-ci-gcp')
               GOOGLE_PROJECT = 'massive-bliss-781'
