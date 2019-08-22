f453b30f1e80dfd5bd49f23f5d593bb7e585a97a
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-08-22 11:08:30 +0200

    fix: corrected tf path for other modules again

--- resources/com/mesosphere/global/create_terraformfile.sh
+++ resources/com/mesosphere/global/create_terraformfile.sh
@@ -3,7 +3,11 @@ set +o xtrace
 set +o errexit
 
 PROVIDER="${1}"
-TF_MODULE_SOURCE="./.."
+if [ ${TF_MODULE_NAME} == "dcos" ]; then
+  TF_MODULE_SOURCE="./.."
+else
+  TF_MODULE_SOURCE="./../../../../.."
+fi
 # we overwrite here the source with the real content of the WORKSPACE as we can rebuild builds in that case
 cat <<EOF | tee Terraformfile
 {
