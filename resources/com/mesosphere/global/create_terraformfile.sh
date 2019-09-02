eb2628509fdab2b6a76d7d4291acd84fbeb94129
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-09-02 11:43:52 +0200

    fix: we still need the relative path here, as terraform will look from inside modules dir for the file

--- resources/com/mesosphere/global/create_terraformfile.sh
+++ resources/com/mesosphere/global/create_terraformfile.sh
@@ -4,7 +4,11 @@ set +o errexit
 
 PROVIDER="${1}"
 ln -sf ${WORKSPACE} symlink_to_workspace
-TF_MODULE_SOURCE="./symlink_to_workspace"
+if [ ${TF_MODULE_NAME} == "dcos" ] || [ ${TF_MODULE_NAME} == "windows-instance" ]; then
+  TF_MODULE_SOURCE="./symlink_to_workspace"
+else
+  TF_MODULE_SOURCE="./../../../../symlink_to_workspace"
+fi
 # we overwrite here the source with the real content of the WORKSPACE as we can rebuild builds in that case
 cat <<EOF | tee Terraformfile
 {
